5f121afebbedbb6a65dd547b88baee2f28785987
Thierry Morre <thierry.morre@cognitic.be>
2025-07-18 10:25:53 +0200

    Ajout commentaire dans la table Utlilisateur

--- DemoHashDb/Tables/Utilisateur.sql
+++ DemoHashDb/Tables/Utilisateur.sql
@@ -2,7 +2,7 @@
 (
 	[Id] INT NOT NULL IDENTITY,
 	[Email] NVARCHAR(384) NOT NULL,
-	[Passwd] BINARY(64) NOT NULL,
+	[Passwd] BINARY(64) NOT NULL, --Car utilisation de l'algorithme de Hashage SHA2_512
 	CONSTRAINT [PK_Utilisateur] PRIMARY KEY ([Id]), 
     CONSTRAINT [UK_Utilisateur_Email_Unique] UNIQUE ([Email])
 )
